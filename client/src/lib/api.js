6548e2850cbb24785f1a81d2980476ddef76b84f
evisa-backend <johnsonsemmanuel@gmail.com>
2026-06-17 10:29:23 +0000

    Fix API baseURL to include /api prefix

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://api.bihlabs.com',
+  baseURL: 'https://api.bihlabs.com/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
